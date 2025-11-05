a4ecd744d83d05006010f68221d0c7d33e709bb3
Rieeel <ldepuuwu14@gmail.com>
2025-11-05 16:03:03 +0700

    Tutorial 7

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:football_news/menu.dart';
+import 'package:football_news/widgets/left_drawer.dart';
+import 'package:football_news/widgets/news_card.dart';
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key});
+    MyHomePage({super.key});
 
-  final String nama = "Lionel Messi"; // nama
-  final String npm = "2406275678"; // npm
-  final String kelas = "B"; // kelas
+    final String nama = "Lionel Messi"; //nama
+    final String npm = "2406275678"; //npm
+    final String kelas = "B"; //kelas
 
-  final List<ItemHomepage> items = [
+    final List<ItemHomepage> items = [
     ItemHomepage("See Football News", Icons.newspaper),
     ItemHomepage("Add News", Icons.add),
     ItemHomepage("Logout", Icons.logout),
-  ];
+    ];
 
-  @override
-  Widget build(BuildContext context) {
+    @override
+    Widget build(BuildContext context) {
     // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
     return Scaffold(
       // AppBar adalah bagian atas halaman yang menampilkan judul.
@@ -31,6 +32,7 @@ class MyHomePage extends StatelessWidget {
         // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
+      drawer: LeftDrawer(),
       // Body halaman dengan padding di sekelilingnya.
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -48,11 +50,16 @@ class MyHomePage extends StatelessWidget {
               ],
             ),
 
+            // Memberikan jarak vertikal 16 unit.
             const SizedBox(height: 16.0),
 
+            // Menempatkan widget berikutnya di tengah halaman.
             Center(
               child: Column(
+                // Menyusun teks dan grid item secara vertikal.
+
                 children: [
+                  // Menampilkan teks sambutan dengan gaya tebal dan ukuran 18.
                   const Padding(
                     padding: EdgeInsets.only(top: 16.0),
                     child: Text(
@@ -71,7 +78,10 @@ class MyHomePage extends StatelessWidget {
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10,
                     crossAxisCount: 3,
+                    // Agar grid menyesuaikan tinggi kontennya.
                     shrinkWrap: true,
+
+                    // Menampilkan ItemCard untuk setiap item dalam list items.
                     children: items.map((ItemHomepage item) {
                       return ItemCard(item);
                     }).toList(),
@@ -86,20 +96,24 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-// Kelas InfoCard
 class InfoCard extends StatelessWidget {
-  final String title;
-  final String content;
+  // Kartu informasi yang menampilkan title dan content.
+
+  final String title;  // Judul kartu.
+  final String content;  // Isi kartu.
 
   const InfoCard({super.key, required this.title, required this.content});
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      // Membuat kotak kartu dengan bayangan dibawahnya.
       elevation: 2.0,
       child: Container(
-        width: MediaQuery.of(context).size.width / 3.5,
+        // Mengatur ukuran dan jarak di dalam kartu.
+        width: MediaQuery.of(context).size.width / 3.5, // menyesuaikan dengan lebar device yang digunakan.
         padding: const EdgeInsets.all(16.0),
+        // Menyusun title dan content secara vertikal.
         child: Column(
           children: [
             Text(
@@ -115,55 +129,9 @@ class InfoCard extends StatelessWidget {
   }
 }
 
-// Kelas ItemHomepage
 class ItemHomepage {
   final String name;
   final IconData icon;
 
   ItemHomepage(this.name, this.icon);
-}
-
-// Kelas ItemCard
-class ItemCard extends StatelessWidget {
-  final ItemHomepage item;
-
-  const ItemCard(this.item, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Theme.of(context).colorScheme.secondary,
-      borderRadius: BorderRadius.circular(12),
-      child: InkWell(
-        onTap: () {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!")),
-            );
-        },
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 }
